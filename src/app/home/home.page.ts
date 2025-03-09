8937ad413381042feddb2e1cbd3af678a6d41dcc
Alessandro Panico <142506840+alessandropanico@users.noreply.github.com>
2025-03-09 13:53:49 +0100

    css

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,8 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
   standalone: false,
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  
+  greetingMessage: string ='';
 
   constructor() {}
 
+  ngOnInit() {
+    this.greetingMessage = this.getGreetingMessage();
+  }
+
+  // Metodo per determinare il messaggio di benvenuto in base all'ora del giorno
+  getGreetingMessage(): string {
+    const currentHour = new Date().getHours();
+
+    if (currentHour < 12) {
+      return 'Buongiorno!';
+    } else if (currentHour < 18) {
+      return 'Buon pomeriggio!';
+    } else {
+      return 'Buona sera!';
+    }
+  }
 }
