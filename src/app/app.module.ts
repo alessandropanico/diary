d7df69c06a4dff773ac1fc8eaa89f57b0f047868
Alessandro Panico <142506840+alessandropanico@users.noreply.github.com>
2025-03-27 14:07:39 +0100

    css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,15 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { IonHeader } from '@ionic/angular';
+import { IonicStorageModule } from '@ionic/storage-angular';
 
 @NgModule({
   declarations: [AppComponent, ],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, ],
+  imports:
+  [BrowserModule,
+  IonicModule.forRoot(), AppRoutingModule,
+  IonicStorageModule.forRoot() // Inizializza lo storage
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent, ],
 })
