8ddeacfe4f5a58697bafa80b64db7a11a768f8e0
Alessandro Panico <142506840+alessandropanico@users.noreply.github.com>
2025-06-15 14:05:30 +0200

    css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { IonHeader } from '@ionic/angular';
 import { IonicStorageModule } from '@ionic/storage-angular';
 
 @NgModule({
