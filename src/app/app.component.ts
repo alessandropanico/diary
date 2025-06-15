9dedf31a511719351c47d67ab9cf12d77fb2c5a6
Alessandro Panico <142506840+alessandropanico@users.noreply.github.com>
2025-06-15 16:22:24 +0200

    css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,25 @@ import { MenuController } from '@ionic/angular';
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
   standalone: false,
+  template: `
+    <button *ngIf="showInstallButton" (click)="installApp()">Installa App</button>
+    <router-outlet></router-outlet>
+  `
 })
 export class AppComponent {
 
-  constructor(private menu: MenuController) { }
+
+  constructor(private menu: MenuController) {
+    window.addEventListener('beforeinstallprompt', (e: any) => {
+      e.preventDefault();
+      this.deferredPrompt = e;
+      this.showInstallButton = true;
+    });
+  }
+
+
+  deferredPrompt: any;
+  showInstallButton = false;
 
   toggleMenu() {
     this.menu.toggle();
@@ -19,5 +34,18 @@ export class AppComponent {
     this.menu.close();  // Chiude il menu dopo il click
   }
 
+  async installApp() {
+    if (!this.deferredPrompt) return;
+
+    this.deferredPrompt.prompt();
+    const choiceResult = await this.deferredPrompt.userChoice;
+    if (choiceResult.outcome === 'accepted') {
+      console.log('Utente ha accettato di installare l\'app');
+    } else {
+      console.log('Utente ha rifiutato l\'installazione');
+    }
+    this.deferredPrompt = null;
+    this.showInstallButton = false;
+  }
 
 }
