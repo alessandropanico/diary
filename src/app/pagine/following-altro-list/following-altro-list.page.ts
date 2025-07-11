4d0b5f87aa53a734ef2be91eab54aa3b3c336ab4
Alessandro Panico <142506840+alessandropanico@users.noreply.github.com>
2025-07-12 00:26:24 +0200

    css

--- src/app/pagine/following-altro-list/following-altro-list.page.ts
+++ src/app/pagine/following-altro-list/following-altro-list.page.ts
@@ -4,6 +4,7 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-following-altro-list',
   templateUrl: './following-altro-list.page.html',
   styleUrls: ['./following-altro-list.page.scss'],
+  standalone: false,
 })
 export class FollowingAltroListPage implements OnInit {
 
