79fba1732e4b092ee3b070e5e085e266cb9b6542
Alessandro Panico <142506840+alessandropanico@users.noreply.github.com>
2025-07-25 17:26:34 +0200

    css

--- src/app/interfaces/comment.ts
+++ src/app/interfaces/comment.ts
@@ -1,20 +1,19 @@
-// src/app/interfaces/comment.ts
 export interface Comment {
   id: string;
   postId: string;
   userId: string;
   username: string;
-  userAvatarUrl: string; // <-- DEVE ESSERE STRING, NON STRING | UNDEFINED
+  userAvatarUrl: string;
   text: string;
-  timestamp: string; // O Date, a seconda di come la gestisci (Firestore.Timestamp è comune)
-  likes: string[]; // Array di user IDs a cui piace
-  replies?: Comment[]; // Opzionale, per i commenti annidati, può essere un array vuoto
-  parentId: string | null; // ID del commento a cui si risponde, null se commento principale
-  isRootComment?: boolean; // Aggiunto per chiarezza, opzionale per il modello dati
+  timestamp: string;
+  likes: string[];
+  replies?: Comment[];
+  parentId: string | null;
+  isRootComment?: boolean;
 }
 
 export interface CommentFetchResult {
   comments: Comment[];
-  lastVisible: any; // o DocumentSnapshot per Firestore
+  lastVisible: any; 
   hasMore: boolean;
 }
