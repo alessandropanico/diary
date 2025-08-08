ba97ab331954b3b0569b2be2d875fc8511a840ac
Alessandro Panico <142506840+alessandropanico@users.noreply.github.com>
2025-08-08 12:41:41 +0200

    css

--- src/app/shared/notifications-modal/notifications-modal.component.ts
+++ src/app/shared/notifications-modal/notifications-modal.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-notifications-modal',
@@ -6,10 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./notifications-modal.component.scss'],
   standalone: false,
 })
-export class NotificationsModalComponent  implements OnInit {
+export class NotificationsModalComponent implements OnInit {
+  // Dati fittizi per testare lo scroll e la visualizzazione
+  mockNotifications = [
+    { title: 'Nuovo messaggio', message: 'Hai un nuovo messaggio da Cloud.', type: 'chat' },
+    { title: 'Sei stato taggato', message: 'Tifa ti ha menzionato in un post.', type: 'mention' },
+    { title: 'Nuova notizia', message: 'Nuovo post nella sezione notizie.', type: 'news' },
+    { title: 'Nuovo messaggio', message: 'Hai un nuovo messaggio da Aerith.', type: 'chat' },
+    { title: 'Nuova notizia', message: 'La Shinra Corporation ha un nuovo annuncio.', type: 'news' },
+    { title: 'Sei stato taggato', message: 'Barret ti ha menzionato in un commento.', type: 'mention' },
+    { title: 'Nuovo messaggio', message: 'Hai un nuovo messaggio da Sephiroth.', type: 'chat' },
+    { title: 'Nuova notizia', message: 'Nuove scoperte sul Lifestream.', type: 'news' },
+    { title: 'Nuovo messaggio', message: 'Hai un nuovo messaggio da Yuffie.', type: 'chat' },
+    { title: 'Sei stato taggato', message: 'Red XIII ti ha menzionato in una discussione.', type: 'mention' },
+    { title: 'Nuovo messaggio', message: 'Hai un nuovo messaggio da Vincent.', type: 'chat' },
+    { title: 'Nuova notizia', message: 'Aggiornamenti dal reattore Mako.', type: 'news' },
+    { title: 'Nuovo messaggio', message: 'Hai un nuovo messaggio da Cid.', type: 'chat' },
+    { title: 'Sei stato taggato', message: 'Zack ti ha menzionato in un ricordo.', type: 'mention' },
+    { title: 'Nuovo messaggio', message: 'Hai un nuovo messaggio da Biggs.', type: 'chat' },
+  ];
 
-  constructor() { }
+  constructor(private modalController: ModalController) { }
 
   ngOnInit() {}
 
+  // Metodo per chiudere la modale
+  dismiss() {
+    this.modalController.dismiss();
+  }
 }
